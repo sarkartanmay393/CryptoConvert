7e846d308dd17e70fc68a72a79759c925f582ee6
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-12-30 19:38:31 +0530

    fixes: baseUrl typo

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const baseUrl = `https://crypto-convert-szaq.vercel.app/`;
+export const baseUrl = `https://crypto-convert-szaq.vercel.app`;
