b76adb0429fea3c4dec62aaff6df35fa781ea2d2
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-12-31 01:01:53 +0530

    feat: removed dummy data usage

--- backend/src/controller/currencyController.ts
+++ backend/src/controller/currencyController.ts
@@ -1,36 +1,37 @@
 import { Request, Response } from "express";
-import Dummy from "../utils/dummy.json";
+// import Dummy from "../utils/dummy.json";
+import { CryptoData, Cryptocurrency } from "src/utils/types";
 
 const getCryptocurrencies = async (req: Request, res: Response) => {
   try {
-    // const response = await fetch(
-    //   "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
-    //   {
-    //     method: "GET",
-    //     headers: {
-    //       "X-CMC_PRO_API_KEY": process.env.CMC_API_KEY || "",
-    //     },
-    //   }
-    // );
+    const response = await fetch(
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+      {
+        method: "GET",
+        headers: {
+          "X-CMC_PRO_API_KEY": process.env.CMC_API_KEY || "",
+        },
+      }
+    );
 
-    // const parsedData: CryptoData = await response.json();
-    // const formatedCurrencies = parsedData.data.map((d: Cryptocurrency) => ({
-    //   id: d.id,
-    //   name: d.name,
-    //   symbol: d.symbol,
-    //   slug: d.slug,
-    //   cmc_rank: d.cmc_rank,
-    //   last_updated: d.last_updated,
-    //   quote: {
-    //     USD: {
-    //       price: d.quote.USD.price,
-    //     },
-    //   },
-    // }));
+    const parsedData: CryptoData = await response.json();
+    const formatedCurrencies = parsedData.data.map((d: Cryptocurrency) => ({
+      id: d.id,
+      name: d.name,
+      symbol: d.symbol,
+      slug: d.slug,
+      cmc_rank: d.cmc_rank,
+      last_updated: d.last_updated,
+      quote: {
+        USD: {
+          price: d.quote.USD.price,
+        },
+      },
+    }));
 
     res.json({
       message: "All cryptocurrencies data fetched!",
-      data: Dummy,
+      data: formatedCurrencies,
     });
   } catch (err) {
     res.json({ message: "operation failed for some reason" });
